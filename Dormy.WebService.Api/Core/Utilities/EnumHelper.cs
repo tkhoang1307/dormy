ecbeb0f7ae580579df19e9fba341f859aa31f413
tkhoang1307 <tkhoanglhpcr@gmail.com>
2025-03-08 23:27:38 +0700

    handle some function relating to RoomServiceTypeEnum

---
+++ Dormy.WebService.Api/Core/Utilities/EnumHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace Dormy.WebService.Api.Core.Utilities
+{
+    public class EnumHelper
+    {
+        public static string GetEnumDescription(Enum value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+            var attribute = (DescriptionAttribute)Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute));
+            return attribute?.Description ?? value.ToString();
+        }
+    }
+}
---
+++ Dormy.WebService.Api/Core/Utilities/RoomServiceTypeEnumHelper.cs
@@ -0,0 +1,30 @@
+﻿using Dormy.WebService.Api.Models.Enums;
+using Dormy.WebService.Api.Models.ResponseModels;
+using System.ComponentModel;
+
+namespace Dormy.WebService.Api.Core.Utilities
+{
+    public class RoomServiceTypeEnumHelper
+    {
+        public static List<RoomServiceTypeResponseModel> GetAllRoomServiceTypes()
+        {
+            return Enum.GetValues(typeof(RoomServiceTypeEnum))
+                       .Cast<RoomServiceTypeEnum>()
+                       .Select(service => GetRoomServiceTypeResponse(service))
+                       .ToList();
+        }
+
+        private static RoomServiceTypeResponseModel GetRoomServiceTypeResponse(RoomServiceTypeEnum serviceType)
+        {
+            var description = EnumHelper.GetEnumDescription(serviceType);
+            var parts = description.Split(" /*-*/ ", StringSplitOptions.None);
+
+            return new RoomServiceTypeResponseModel
+            {
+                RoomServiceType = serviceType.ToString(),
+                VietnameseRoomServiceTypeName = parts.Length > 1 ? parts[1] : string.Empty,
+                EnglishRoomServiceTypeName = parts.Length > 0 ? parts[0] : string.Empty
+            };
+        }
+    }
+}
