5b3bdbe23d4d40a901175b5e8eedeec785671d07
tkhoang1307 <tkhoanglhpcr@gmail.com>
2025-03-09 16:00:52 +0700

    remove some endpoints relating to registration

--- Dormy.WebService.Api/Models/RequestModels/RegisterRequestModel.cs
+++ Dormy.WebService.Api/Models/RequestModels/RegisterRequestModel.cs
@@ -4,22 +4,10 @@
     {
         public UserRegisterRequestModel User { get; set; } = null!;
         public Guid? WorkplaceId { get; set; }
-        public Guid RoomTypeId { get; set; }
+        public Guid RoomId { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public HealthInsuranceRequestModel HealthInsurance { get; set; } = null!;
-        public List<GuardianRequestModel>? Guardians { get; set; }
-        public List<VehicleRequestModel>? Vehicles { get; set; }
-    }
-
-    public class RegisterUpdateRequestModel
-    {
-        public UserRegisterRequestModel User { get; set; } = null!;
-        public Guid? WorkplaceId { get; set; }
-        public Guid RoomTypeId { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-        public HealthInsuranceUpdationRequestModel HealthInsurance { get; set; } = null!;
         public List<GuardianRequestModel>? Guardians { get; set; }
         public List<VehicleRequestModel>? Vehicles { get; set; }
     }
--- Dormy.WebService.Api/Presentation/Controllers/RegistrationController.cs
+++ Dormy.WebService.Api/Presentation/Controllers/RegistrationController.cs
@@ -17,42 +17,11 @@ namespace Dormy.WebService.Api.Presentation.Controllers
         }
 
         [HttpPost]
-        [Authorize(Roles = Role.USER)]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> Register(RegisterRequestModel model)
         {
             var result = await _contractService.Register(model);
-            return StatusCode((int)result.StatusCode, result);
-        }
-
-        [HttpGet("requestId/{requestId:guid}/userId/{userId:guid}")]
-        [Authorize(Roles = $"{Role.USER}, {Role.ADMIN}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> GetRegistrationByRequestId(Guid requestId, Guid? userId)
-        {
-            var result = await _contractService.GetRegistrationByRequestId(requestId: requestId, userId: userId);
-            return StatusCode((int)result.StatusCode, result);
-        }
-
-        [HttpPost("batch/userId/{userId:guid}")]
-        [Authorize(Roles = $"{Role.USER}, {Role.ADMIN}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> GetRegistrationBatchByRequestIds(GetBatchRequestModel model, Guid? userId)
-        {
-            var result = await _contractService.GetRegistrationBatchByRequestIds(model.Ids, userId: userId);
-            return StatusCode((int)result.StatusCode, result);
-        }
-
-        [HttpPut("requestId/{requestId:guid}")]
-        [Authorize(Roles = Role.USER)]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> UpdateRegistrationByRequestId(Guid requestId, RegisterUpdateRequestModel model)
-        {
-            var result = await _contractService.UpdateRegistration(requestId, model);
             return StatusCode((int)result.StatusCode, result);
         }
     }
