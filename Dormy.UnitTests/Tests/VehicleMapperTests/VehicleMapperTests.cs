65f44d29ff3cd67edce9b4851f4eec6fe2b5b2d2
DESKTOP-VNDPGJ4\KhoaHT <lilo190516@gmail.com>
2025-04-13 21:47:04 +0700

    add unit test

---
+++ Dormy.UnitTests/Tests/VehicleMapperTests/VehicleMapperTests.cs
@@ -0,0 +1,37 @@
+﻿
+using Dormy.WebService.Api.Core.Entities;
+using Dormy.WebService.Api.Presentation.Mappers;
+
+namespace Dormy.UnitTests.Tests.VehicleMapperTests
+{
+    public class VehicleMapperTests
+    {
+        private readonly VehicleMapper _mapper;
+
+        public VehicleMapperTests()
+        {
+            _mapper = new VehicleMapper();
+        }
+
+        [Fact]
+        public void TestVehicleMapper_Success()
+        {
+            VehicleEntity data = new VehicleEntity
+            {
+                Id = Guid.Empty,
+                VehicleType = "SUV",
+                User = new UserEntity
+                {
+                    FirstName = "Adam",
+                    LastName = "John"
+                }
+            };
+
+            var result = _mapper.MapToVehicleResponseModel(data);
+
+            Assert.NotNull(result);
+            Assert.Equal("SUV", result.VehicleType);
+            Assert.Equal("Adam John", result.UserFullname);
+        }
+    }
+}
